9b107c14c720e9df4ce05214db4513424a0fbed4
Maksym Kravchuk <kravchukmaxym@gmail.com>
2020-11-15 12:42:39 +0200

    Added form handler

--- form.php
+++ form.php
@@ -1,32 +1,25 @@
 <?php 
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
+  header("Content-type: text/html; charset=utf-8");
 
-$name = $_POST['name'];
-$phone = $_POST['phone'];
+  $to1 = "kravchukmaxym@gmail.com";
+  $subject = "Обращение с сайта ". date("Y-m-d H:i:s");
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+  $message = "";
+  foreach ($_POST as $item) {
+    if(is_array($item)) {
+      foreach ($item as $dd) {
+        $message .= '<p>'.$dd.'</p>';
+      }
+    } else {
+      $message .= '<p>'.$item.'</p>';
+    }
+  }
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'kravchukmaxym@gmail.com';          // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'Rhfdxer1988';                      // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+  $headers = "MIME-Version: 1.0\r\n";
+  $headers .= "Content-type: text/html; charset=utf8\r\n";
 
-$mail->setFrom('kravchukmaxym@gmail.com');            // От кого будет уходить письмо?
-$mail->addAddress('kravchukmaxym@gmail.com');         // Кому будет уходить письмо
-$mail->isHTML(true);                                  // Set email format to HTML
+  mail($to1, $subject, $message, $headers);
 
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name. ' оставил заявку, его телефон ' .$phone;
-$mail->AltBody = '';
-
-if($mail->send()) {
-  echo 'Mail send';
-} else {
-  echo 'Error';
-}
+  header("Content-type: application/json");
+  echo json_encode(['message' => 'Mail sent.'])
 ?>
